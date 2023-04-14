599550bc549681ecfa55606c3fa0656908a848eb
Alick <alick.yang96@gmail.com>
2023-04-14 14:28:33 +1000

    Create Accordion component

---
+++ src/components/UI/ExpandingCard.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Card from './Card'
+import { IoIosArrowDropdown } from 'react-icons/io'
+const ExpandingCard = (props) => {
+    const { children, header, media } = props
+    const [isActive, setIsActive] = useState(false)
+    const onActiveHandler = () => {
+        setIsActive(!isActive)
+    }
+    return (
+        <>
+            <div className="group">
+                <Card media={media} header={header} effects={""}>
+                    <button onClick={onActiveHandler}>
+                        {isActive ? <IoIosArrowDropdown className="animate-bounce mx-auto text-xl transform:rotateX(180deg)" /> : <IoIosArrowDropdown className="animate-bounce mx-auto text-xl" />}
+                    </button>
+                    <div className={`overflow-hidden font-bold transition-[max-height] duration-300 ease-in ${isActive ? "max-h-40" : "max-h-0"}`}>
+
+                        {children}
+                    </div>
+                </Card >
+
+            </div >
+        </>
+    )
+}
+
+
+export default ExpandingCard
