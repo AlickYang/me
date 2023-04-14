6d17ccd49d3c9dc59238dd795dfe8dadde8a6470
Alick <alick.yang96@gmail.com>
2023-04-14 15:59:20 +1000

    Add bouncing animation to arrow

--- src/components/UI/ExpandingCard.js
+++ src/components/UI/ExpandingCard.js
@@ -13,10 +13,9 @@ const ExpandingCard = (props) => {
             <div className="group">
                 <Card media={media} header={header} effects={""}>
                     <button onClick={onActiveHandler}>
-                        {isActive ? <IoIosArrowDropdown className="animate-bounce mx-auto text-xl transform:rotateX(180deg)" /> : <IoIosArrowDropdown className="animate-bounce mx-auto text-xl" />}
+                        {isActive ? <IoIosArrowDropdown className="mx-auto text-xl rotate-180" /> : <IoIosArrowDropdown className="animate-bounce mx-auto text-xl" />}
                     </button>
                     <div className={`overflow-hidden font-bold transition-[max-height] duration-300 ease-in ${isActive ? "max-h-40" : "max-h-0"}`}>
-
                         {children}
                     </div>
                 </Card >
